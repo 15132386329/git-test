751fcfebf89eaa2aab4c2e89fb94a31c8a0afa0d
Penance <1070594335@qq.com>
2022-05-11 11:14:22 +0800

    first commit

---
+++ src/main/java/com/hyz/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.hyz.git;
+
+/**
+ * @author Lenovo
+ * @create 2022-05-11 11:10
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
